7c91e5c3167b0db18d838c10fdb6789aa9b3c20d
bautibonasso-arch <bautibonasso@gmail.com>
2025-08-27 12:05:57 -0300

    Add files via upload

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full=f"{first_name} {last_name}"
+    print(full.lower())
+    print(full.title())
+    print(full.upper())
+ada()
+
--- change.py
+++ change.py
@@ -1,3 +1,17 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto=round((money-expense)*100)
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(vuelto//100)
+    print("Centavos:")
+    print(vuelto%100)
+change()
+
